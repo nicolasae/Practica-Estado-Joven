c100c3812fd810df30d8c097e5d7d1ff82d373c5
nicolas aguirre <nicolas.aguirre@utp.edu.co>
2021-08-26 11:43:22 -0500

    wip

---
+++ frontend/coreui-free-react-admin-template-master/src/containers/TheHeader.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  CHeader,
+  CToggler,
+  CHeaderBrand,
+  CHeaderNav,
+  CHeaderNavItem,
+  CHeaderNavLink,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+const TheHeader = () => {
+  const dispatch = useDispatch()
+  const sidebarShow = useSelector(state => state.sidebarShow)
+
+  const toggleSidebar = () => {
+    const val = [true, 'responsive'].includes(sidebarShow) ? false : 'responsive'
+    dispatch({type: 'set', sidebarShow: val})
+  }
+
+  const toggleSidebarMobile = () => {
+    const val = [false, 'responsive'].includes(sidebarShow) ? true : 'responsive'
+    dispatch({type: 'set', sidebarShow: val})
+  }
+
+  return (
+    <CHeader withSubheader>
+      <CToggler
+        inHeader
+        className="ml-md-3 d-lg-none"
+        onClick={toggleSidebarMobile}
+      />
+      <CToggler
+        inHeader
+        className="ml-3 d-md-down-none"
+        onClick={toggleSidebar}
+      />
+      <CHeaderBrand className="mx-auto d-lg-none" to="/">
+        <CIcon name="logo" height="48" alt="Logo"/>
+      </CHeaderBrand>
+
+      <CHeaderNav className="d-md-down-none mr-auto">
+        <CHeaderNavItem className="px-3" >
+          <CHeaderNavLink to="/dashboard">Inicio</CHeaderNavLink>
+        </CHeaderNavItem>
+        <CHeaderNavItem  className="px-3">
+          <CHeaderNavLink to="/users">Pregrado</CHeaderNavLink>
+        </CHeaderNavItem>
+        <CHeaderNavItem className="px-3">
+          <CHeaderNavLink>Posgrado</CHeaderNavLink>
+        </CHeaderNavItem>
+      </CHeaderNav>
+    </CHeader>
+  )
+}
+
+export default TheHeader
